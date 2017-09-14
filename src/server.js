6c1af1afdb0b8223f996e8de5ecc306a6496538f
Bhavani Shankar <ebhavanishankar@gmail.com>
2017-09-14 12:03:41 +0530

    change pathName to pathname

--- src/server.js
+++ src/server.js
@@ -6,29 +6,29 @@ const port = process.argv[2] || 9000
 
 http.createServer(function (req, res) {
   const parsedUrl = url.parse(req.url)
-  let pathName = `.${parsedUrl.pathName}`
+  let pathname = `.${parsedUrl.pathname}`
   const mimeType = {
     '.html': 'text/html',
     '.json': 'application/json'
   }
 
-  fs.exists(pathName, function (exists) {
-    if (!exists) {
+  fs.exists(pathname, function (exist) {
+    if (!exist) {
       res.statusCode = 404
-      res.end(`File ${pathName} not found!`)
+      res.end(`File ${pathname} not found!`)
       return
     }
 
-    if (fs.statSync(pathName).isDirectory()) {
-      pathName += '/index.html'
+    if (fs.statSync(pathname).isDirectory()) {
+      pathname += '/index.html'
     }
 
-    fs.readFile(pathName, function (err, data) {
+    fs.readFile(pathname, function (err, data) {
       if (err) {
         res.statusCode = 500
         res.end(`Error getting the file: ${err}.`)
       } else {
-        const ext = path.parse(pathName).ext
+        const ext = path.parse(pathname).ext
         res.setHeader('Content-type', mimeType[ext] || 'text/plain')
         res.end(data)
       }
