37b33d4c837a26367b3358e18528fb65a089f9ba
Bhavani Shankar <ebhavanishankar@gmail.com>
2017-09-14 15:59:20 +0530

    simplify server.js a bit

--- src/server.js
+++ src/server.js
@@ -4,6 +4,16 @@ const fs = require('fs')
 const path = require('path')
 const port = process.argv[2] || 9000
 
+function putJson (req) {
+  let body = []
+  req.on('data', (chunk) => {
+    body.push(chunk)
+  }).on('end', () => {
+    body = Buffer.concat(body).toString()
+    if (body !== '') fs.writeFile('./tasks.json', body, 'utf8', null)
+  })
+}
+
 http.createServer(function (req, res) {
   const parsedUrl = url.parse(req.url)
   let pathname = `.${parsedUrl.pathname}`
@@ -11,37 +21,16 @@ http.createServer(function (req, res) {
     '.html': 'text/html',
     '.json': 'application/json'
   }
-
-  if (req.method === 'PUT') {
-    let body = []
-    req.on('data', (chunk) => {
-      body.push(chunk)
-    }).on('end', () => {
-      body = Buffer.concat(body).toString()
-      if (body !== '') fs.writeFile('./tasks.json', body, 'utf8', null)
-    })
+  if (req.method === 'PUT') putJson(req)
+  if (fs.statSync(pathname).isDirectory()) {
+    pathname += 'index.html'
   }
-
-  fs.exists(pathname, function (exist) {
-    if (!exist) {
-      res.statusCode = 404
-      res.end(`File ${pathname} not found!`)
-      return
+  fs.readFile(pathname, function (err, data) {
+    if (err) throw err
+    else {
+      const ext = path.parse(pathname).ext
+      res.setHeader('Content-type', mimeType[ext] || 'text/plain')
+      res.end(data)
     }
-
-    if (fs.statSync(pathname).isDirectory()) {
-      pathname += '/index.html'
-    }
-
-    fs.readFile(pathname, function (err, data) {
-      if (err) {
-        res.statusCode = 500
-        res.end(`Error getting the file: ${err}.`)
-      } else {
-        const ext = path.parse(pathname).ext
-        res.setHeader('Content-type', mimeType[ext] || 'text/plain')
-        res.end(data)
-      }
-    })
   })
 }).listen(parseInt(port))
